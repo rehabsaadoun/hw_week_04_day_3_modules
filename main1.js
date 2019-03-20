e3ea8a7cef274f5e008cd0ee6d75c21b58b5c54a
rehabsaadoun <YOUR GITHUB EMAIL>
2019-03-20 16:32:18 +0300

    week4-hw3

---
+++ main1.js
@@ -0,0 +1,25 @@
+ var sayHello = function(name)
+{console.log("Hi,${name}!");
+}
+var sayHello = (name)=>
+console.log("Hi,${name}!");
+
+/////////////////////////////////////////////
+
+
+const getOlder= function(age)
+{return age +50;}
+const getOlder = (age)=>age+50;
+
+/////////////////////////////////////////////
+
+
+const multiply = (num1,mun2) =>
+{console.log(num1*num2)
+};
+
+const subtractFive = num=>5-num;
+console.log(subtractFive);
+const printName= function(name){
+    console.log(name);
+}
