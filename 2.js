7c9bef52aedb794aa32bc5764491669cad69e65d
rehabsaadoun <YOUR GITHUB EMAIL>
2019-03-20 06:20:38 +0300

    rehab

---
+++ 2.js
@@ -0,0 +1,11 @@
+person.sayHello("Anoud") //should return Hi, Anoud!
+
+person.getOlder(12) //should return 62 
+
+const sayHello = require('./Anoud')
+
+console.log(Hi.Anoud)
+/////////////////////////////////////////
+const getOlder = require('./50+12')
+
+console.log(62)
